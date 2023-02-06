b2efd3c4513887615e5c8005473e0211aa2ef1a5
KingCoder-pixel <77515676+KingCoder-pixel@users.noreply.github.com>
2023-02-05 19:42:28 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify, request
+import csv
+all_movies = []
+
+#fetching data
+with open('movies.csv','r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_movies = data[1:]
+
+#creating empty list
+like_movies = []
+notLike_movies = []
+
+
+#creating object of flask
+app = Flask(__name__)
+
+#first api to get all movies detail
+@app.route("/getMovie")
+def get_movie():
+    return jsonify({
+        "data": all_movies[0],
+        "status": "success"
+    })
+    
+
+#api to like movies
+@app.route("/like_movie", methods = ["POST"])
+def liked_Movie():
+    movies = all_movies[0]
+    all_movies = all_movies[1:]
+    liked_Movie.append(movies)
+    return jsonify({
+        "status": "success"
+    }),201
+
+@app.route("/Unliked_Movie", methods = ["POST"])
+def notLiked_Movies():
+    movies = all_movies[0]
+    all_movies = all_movies[1:]
+    notLiked_Movies.append(movies)
+    return jsonify({
+        "status": "success"
+    }),201
+
+
+
+#to execute api
+
+if __name__ == "__main__":
+  app.run()
+
+
+
